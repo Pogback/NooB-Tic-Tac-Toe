54405242f25c0c95f5bbfd9c0adecc8697776380
Pogback <adzagaming@gmail.com>
2017-09-08 23:10:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -130,7 +130,11 @@ int main()
         cout<<"O je pobjednik"<<endl;
         break;
 
+      }else if((p1&p2&p3&p4&p5&p6&p7&p8&p9)!=NULL){
+        cout<<"Izjednaceno je"<<endl;
+        break;
       }
+
 
      }
 
